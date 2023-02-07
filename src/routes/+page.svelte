4a68217bbe738313362ce1f5bfb810c2138f7bdc
tglide <26071571+TGlide@users.noreply.github.com>
2023-02-07 19:21:32 +0000

    feat: show menu on click

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { sineInOut } from 'svelte/easing';
+
 	type MenuItem = {
 		icon: string;
 		label: string;
 	};
 
 	const menuItems = [
-		{ icon: 'printer', label: 'Print' },
+		{ icon: 'adjustments-horizontal', label: 'Settings' },
 		{ icon: 'arrow-left', label: 'Back' },
-		{ icon: 'printer', label: 'print' },
+		{ icon: 'printer', label: 'Print' },
 		{ icon: 'reload', label: 'Reload' },
 		{ icon: 'arrow-right', label: 'Forward' },
-		{ icon: 'printer', label: 'print' },
-		{ icon: 'printer', label: 'print' },
-		{ icon: 'printer', label: 'print' },
+		{ icon: 'activity', label: 'Activity' },
+		{ icon: 'printer', label: 'Print' },
+		{ icon: 'printer', label: 'Print' },
 	] satisfies MenuItem[];
 
 	let numItems = 6;
 	let selected: number | null = null;
+	let mouseCoords: [number, number] | null = null;
 
 	$: [skew, top, left] = (function getProperties() {
 		switch (numItems) {
@@ -37,32 +41,54 @@
 		}
 	})();
 
-	$: style = [`--skew: ${skew}deg`, `--top: ${top}%`, `--left: ${left}%`].join(';');
+	$: style = [
+		`--skew: ${skew}deg`,
+		`--top: ${top}%`,
+		`--left: ${left}%`,
+		`--mouseX: ${mouseCoords ? mouseCoords[0] : 0}px`,
+		`--mouseY: ${mouseCoords ? mouseCoords[1] : 0}px`,
+	].join(';');
 </script>
+
+<svelte:window
+	on:mousedown={(e) => {
+		mouseCoords = [e.clientX, e.clientY];
+	}}
+	on:mouseup={() => {
+		mouseCoords = null;
+		selected = null;
+	}}
+/>
 
 <div class="input-wrapper">
 	<input type="range" min="3" max={menuItems.length} bind:value={numItems} />
 </div>
 
-<ul class="radial-menu" {style}>
-	{#each menuItems.slice(0, numItems) as item, i}
-		{@const rotate = (360 / numItems) * i - 90}
-		<li
-			class="item"
-			role="button"
-			style:--rotate={`${rotate}deg`}
-			on:mouseover={() => (selected = i)}
-			on:focus={() => (selected = i)}
-			on:mouseout={() => (selected = null)}
-			on:blur={() => (selected = null)}
-		>
-			<i class={`ti ti-${item.icon}`} />
-		</li>
-	{/each}
-	{#if selected !== null}
-		<span class="label">{menuItems[selected].label}</span>
-	{/if}
-</ul>
+{#if mouseCoords}
+	<ul
+		class="radial-menu"
+		{style}
+		transition:scale={{ start: 0.9, duration: 150, easing: sineInOut }}
+	>
+		{#each menuItems.slice(0, numItems) as item, i}
+			{@const rotate = (360 / numItems) * i - 90}
+			<li
+				class="item"
+				style:--rotate={`${rotate}deg`}
+				data-selected={selected === i}
+				on:mouseover={() => (selected = i)}
+				on:focus={() => (selected = i)}
+			>
+				<i class={`ti ti-${item.icon}`} />
+			</li>
+		{/each}
+		<div class="inner" on:mouseover={() => (selected = null)} on:focus={() => (selected = null)}>
+			{#if selected !== null}
+				<span class="label">{menuItems[selected].label}</span>
+			{/if}
+		</div>
+	</ul>
+{/if}
 
 <style lang="scss">
 	.input-wrapper {
@@ -87,13 +113,13 @@
 		height: $size;
 
 		position: absolute;
-		top: 50%;
-		left: 50%;
+		top: var(--mouseY);
+		left: var(--mouseX);
 		translate: -50% -50%;
 
-		&::after {
-			content: '';
+		user-select: none;
 
+		.inner {
 			background-color: var(--color-gray-12);
 			border-radius: var(--radius-full);
 
@@ -106,46 +132,45 @@
 			width: $innerSize;
 			height: $innerSize;
 		}
-	}
 
-	.label {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		translate: -50% -50%;
-		z-index: 999;
-	}
-
-	.item {
-		display: block;
-		$itemSize: toRem(200);
-		width: $itemSize;
-		height: $itemSize;
-
-		position: absolute;
-		bottom: 50%;
-		right: 50%;
-
-		background-color: var(--color-gray-11);
-		border-right: 1px solid var(--color-gray-10);
-
-		transform-origin: 100% 100% 0px;
-		transform: rotate(var(--rotate)) skew(var(--skew));
-
-		transition: background 200ms ease;
-
-		&:hover {
-			background-color: var(--color-gray-10);
-			cursor: pointer;
+		.label {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			translate: -50% -50%;
+			z-index: 999;
 		}
 
-		i {
-			position: absolute;
-			top: var(--top);
-			left: var(--left);
+		.item {
+			display: block;
+			$itemSize: toRem(200);
+			width: $itemSize;
+			height: $itemSize;
 
-			transform: skew(calc(var(--skew) * -1)) rotate(calc(var(--rotate) * -1));
-			font-size: 1.5rem;
+			position: absolute;
+			bottom: 50%;
+			right: 50%;
+
+			background-color: var(--color-gray-11);
+			border-right: 1px solid var(--color-gray-10);
+
+			transform-origin: 100% 100% 0px;
+			transform: rotate(var(--rotate)) skew(var(--skew));
+
+			transition: background 200ms ease;
+
+			&[data-selected='true'] {
+				background-color: var(--color-gray-10);
+			}
+
+			i {
+				position: absolute;
+				top: var(--top);
+				left: var(--left);
+
+				transform: skew(calc(var(--skew) * -1)) rotate(calc(var(--rotate) * -1));
+				font-size: 1.5rem;
+			}
 		}
 	}
 </style>
